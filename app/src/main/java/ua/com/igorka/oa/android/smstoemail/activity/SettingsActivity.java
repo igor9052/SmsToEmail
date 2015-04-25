074e7b244cce895a1e135f2c7af439769a64594d
igor9052 <igor905.2@gmail.com>
2015-04-25 14:14:53 +0300

    Setting activity was improved. We can see summary settings now.

--- app/src/main/java/ua/com/igorka/oa/android/smstoemail/activity/SettingsActivity.java
+++ app/src/main/java/ua/com/igorka/oa/android/smstoemail/activity/SettingsActivity.java
@@ -2,8 +2,15 @@ package ua.com.igorka.oa.android.smstoemail.activity;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.EditTextPreference;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.PreferenceCategory;
 import android.preference.PreferenceFragment;
+import android.text.Editable;
+import android.text.InputType;
 import android.util.Log;
 
 import ua.com.igorka.oa.android.smstoemail.R;
@@ -42,7 +49,7 @@ public class SettingsActivity extends Activity {
         }
     }
 
-    public static class SettingsFragment extends PreferenceFragment {
+    public static class SettingsFragment extends PreferenceFragment implements SharedPreferences.OnSharedPreferenceChangeListener {
 
         public static final String TAG = SettingsFragment.class.getSimpleName();
 
@@ -58,6 +65,64 @@ public class SettingsActivity extends Activity {
             Log.i(TAG, "ON_DESTROY");
         }
 
+        @Override
+        public void onResume() {
+            super.onResume();
+            getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
+            initSummary();
+        }
+
+        @Override
+        public void onPause() {
+            super.onPause();
+            getPreferenceScreen().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(this);
+        }
+
+        @Override
+        public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+            updatePrefsSummary(findPreference(key));
+        }
+
+        protected void initSummary() {
+            for (int i = 0; i < getPreferenceScreen().getPreferenceCount(); i++) {
+                initPrefsSummary(getPreferenceScreen().getPreference(i));
+            }
+        }
+
+        /*
+         * Init single Preference
+         */
+        protected void initPrefsSummary(Preference p) {
+            if (p instanceof PreferenceCategory) {
+                PreferenceCategory pCat = (PreferenceCategory) p;
+                for (int i = 0; i < pCat.getPreferenceCount(); i++) {
+                    initPrefsSummary(pCat.getPreference(i));
+                }
+            } else {
+                updatePrefsSummary(p);
+            }
+        }
+
+        protected void updatePrefsSummary(Preference pref) {
+            if (pref == null)
+                return;
+
+            if (pref instanceof ListPreference) {
+                // List Preference
+                ListPreference listPref = (ListPreference) pref;
+                listPref.setSummary(listPref.getEntry());
+
+            } else if (pref instanceof EditTextPreference) {
+                // EditPreference
+                EditTextPreference editTextPref = (EditTextPreference) pref;
+                if ((editTextPref.getEditText().getInputType() & 0x80)  == InputType.TYPE_TEXT_VARIATION_PASSWORD) {
+                    editTextPref.setSummary("********");
+                    return;
+                }
+                editTextPref.setSummary(editTextPref.getText());
+            }
+
+        }
     }
 
 
--- app/src/main/java/ua/com/igorka/oa/android/smstoemail/receiver/SmsBroadcastReceiver.java
+++ app/src/main/java/ua/com/igorka/oa/android/smstoemail/receiver/SmsBroadcastReceiver.java
@@ -3,6 +3,7 @@ package ua.com.igorka.oa.android.smstoemail.receiver;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 
 import ua.com.igorka.oa.android.smstoemail.service.SendEmailService;
 
@@ -11,10 +12,12 @@ import ua.com.igorka.oa.android.smstoemail.service.SendEmailService;
  * This broadcast receiver gets SMS messages and sends them to SendEmailService class
  */
 public class SmsBroadcastReceiver extends BroadcastReceiver {
+    public static final String TAG = SmsBroadcastReceiver.class.getSimpleName();
 
     @Override
     public void onReceive(Context context, Intent intent) {
         intent.setClass(context, SendEmailService.class);
+        Log.d(TAG, "ON_RECEIVE");
         context.startService(intent);
     }
 }
